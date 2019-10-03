56a62d615d0291faea587242887e5df07b165ca5
Sopri00933 <54856836+Sopri00933@users.noreply.github.com>
2019-10-03 16:19:45 +0300

    gönder

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta charset = "utf-8"/>
+
+<title>SOPRİ SOFTWARE</title>
+
+<link rel ="stylesheet" href ="setting.css"/>
+
+</head>
+<body>
+
+<form>
+
+<div class="header">
+<h1 class="header"><center>SOPRİ SOFTWARE</center></h1><br/> 
+
+</div>
+
+
+<article>
+<p><strong>Java Android Programlama</strong></p><br/>
+<p><strong>C#</strong> ile Hastane Otomasyounu , Market Programı , Otel Otomasyonu </p><br/>
+<p><strong>PHP , HTML5 , CSS3 , JAVASCRİPT</strong> ile Web Sayfası ve Haber Siteleri hizmeti vermekteyiz</p><br/>
+</article>
+
+
+<aside id="solmenu">
+
+</aside>
+
+<nav> 
+<p><center><strong>İletişim Bilgileri</strong></center></p>
+<p><img  src = "images/08_phone-128.png" alt = "Cep Telefonu" width = "25"  height = "25" title = "Cep Telefonu">  05310875447</p><br/>  
+<p><img  src = "images/email-128.png" alt = "E-mail Adresi" width = "25"  height = "25" title = "E-mail Adresi">  Sopri17Software@gmail.com</p><br/> 
+<p><img  src = "images/48-128.png" alt = "Adres" width = "25"  height = "25" title = "Adres">  Hatay Antakya</p><br/>  
+</nav>  
+
+<footer>
+
+</footer>
+
+</form>
+
+</body>
+</html>
